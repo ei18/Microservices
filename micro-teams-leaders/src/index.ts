7b7c0f0a84fdf51bc3b9f038aa9b9642b658b64a
Keity <keityortegam@gmail.com>
2024-07-24 13:05:15 -0500

    Microservices finished

--- micro-teams-leaders/src/index.ts
+++ micro-teams-leaders/src/index.ts
@@ -1,13 +1,30 @@
 import express, { Request, Response } from "express";
+import { AppDataSource } from "./config/connection-db";
+import teamLeaderRouter from "./router/team-leader.route";
+import { client } from "./config/eureka-client";
+import { error } from "console";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response)=> {
     res.json({ message: "Hello World!" });
 })
 
+app.use("/api/v1/team-leader", teamLeaderRouter);
 
-app.listen(7000, () => {
-    console.log("Server is running on port 7000");
-})
+AppDataSource
+    .initialize()
+    .then(() => {
+        console.log("Database connected");
+        
+        app.listen(7000, ()=>{
+            console.log("Server is running on port 7000");
+            client.start(error => {
+                console.log(error || "Eureka Client started");
+            });
+        })
+    })
+    .catch((err) => {
+        console.log("Error connecting to database", err);
+    })
